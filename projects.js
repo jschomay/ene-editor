c9720b3d0e7fa69e2ebf06ac02ed70becdefcda8
Jeff Schomay <jschomay@gmail.com>
2020-06-17 09:29:23 -0600

    add auth for projects page
    still need to do edit page

--- projects.js
+++ projects.js
@@ -1,37 +1,11 @@
-(() => {
-  const $projects = $("#projects");
-  const $projectDetails = $("#project-details");
-  const $newProjectModal = $("#new-project-modal");
+window.ENE = window.ENE || {};
+window.ENE.projects = {
+  init: () => {
+    const $projects = $("#projects");
+    const $projectDetails = $("#project-details");
+    const $newProjectModal = $("#new-project-modal");
 
-  $("#create-new-project").click(function() {
-    $(this).attr("disabled", true);
-    let originalButtonText = $(this).text();
-    $(this).text("Creating...");
-    let name = $projectDetails.find("#project-name").val() || "My project";
-    let description = $projectDetails.find("#project-description").val();
-    firebase
-      .firestore()
-      .collection("projects")
-      .add({ name, description })
-      .then(doc => {
-        // window.location.href = `/edit?id=${doc.id}`;
-
-        $projectDetails.find("#project-name").val("");
-        $projectDetails.find("#project-description").val("");
-        $(this).attr("disabled", false);
-        $(this).text(originalButtonText);
-        $newProjectModal.modal("hide");
-        getProjects();
-      })
-      .catch(e => {
-        console.error("Error creating project", e);
-        alert("Error creating project, please refresh and try again.");
-      });
-  });
-
-  getProjects();
-
-  function getProjects() {
+    // fetch projects
     firebase
       .firestore()
       .collection("projects")
@@ -40,16 +14,42 @@
         $projects.empty();
         snapshot.forEach(renderProject);
       });
-  }
 
-  function renderProjectItem({ name, description, id }) {
-    return $(`<a href="edit.html?id=${id}" class="list-group-item list-group-item-action">
+    $("#create-new-project").click(function() {
+      $(this).attr("disabled", true);
+      let originalButtonText = $(this).text();
+      $(this).text("Creating...");
+      let name = $projectDetails.find("#project-name").val() || "My project";
+      let description = $projectDetails.find("#project-description").val();
+      firebase
+        .firestore()
+        .collection("projects")
+        .add({ name, description })
+        .then(doc => {
+          // need to empty out form or browser remembers it
+          $projectDetails.find("#project-name").val("");
+          $projectDetails.find("#project-description").val("");
+          $(this).attr("disabled", false);
+          $(this).text(originalButtonText);
+          $newProjectModal.modal("hide");
+
+          window.location.href = `/edit.html?id=${doc.id}`;
+        })
+        .catch(e => {
+          console.error("Error creating project", e);
+          alert("Error creating project, please refresh and try again.");
+        });
+    });
+
+    function renderProjectItem({ name, description, id }) {
+      return $(`<a href="edit.html?id=${id}" class="list-group-item list-group-item-action">
                 <h5 class="mb-1">${name || "My project"}</h5>
                 <p class="mb-1">${description || ""}</p>
               </a>`);
-  }
+    }
 
-  function renderProject(doc) {
-    $projects.append(renderProjectItem({ ...doc.data(), id: doc.id }));
+    function renderProject(doc) {
+      $projects.append(renderProjectItem({ ...doc.data(), id: doc.id }));
+    }
   }
-})();
+};
