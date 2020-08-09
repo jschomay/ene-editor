51c53d0cd5b01ef99338bb7e76acf96c933c30b3
Jeff Schomay <jschomay@gmail.com>
2020-08-09 15:07:33 -0600

    improve cell editing
    - esc to cancel
    - shift enter to save in text area
    - shift tab for prev (only in text areas)
    - update hights dynamically

--- editor.js
+++ editor.js
@@ -73,6 +73,7 @@ window.ENE.Editor = {
       //create and style input
       editor.style.padding = "3px";
       editor.style.width = "100%";
+      editor.style.height = "100%";
       editor.style.boxSizing = "border-box";
 
       //Set value of editor to the current value of the cell
@@ -81,16 +82,78 @@ window.ENE.Editor = {
       //set focus on the select box when the editor is selected (timeout allows for editor to be added to DOM)
       onRendered(function() {
         ENE.Completion.addAutocomplete(editor, isRule);
+        editor.style.height = "100%";
+        if (isRule) {
+          editor.style.minHeight = "7em";
+          cell.getRow().normalizeHeight();
+        }
         editor.focus();
       });
 
       //when the value has been set, trigger the cell to update
       function successFunc() {
         success(editor.value);
+        cell.getRow().normalizeHeight();
       }
 
       editor.addEventListener("change", successFunc);
       editor.addEventListener("blur", successFunc);
+      editor.addEventListener("keyup", e => {
+        // esc
+        if (e.keyCode === 27) cancel();
+        // shift + enter
+        if (e.keyCode === 13 && e.shiftKey) success(editor.value);
+        // shift + tab
+        if (e.keyCode === 9 && e.shiftKey) cell.nav().left();
+      });
+
+      //return the editor element
+      return editor;
+    };
+
+    var textAreaEditor = (cell, onRendered, success, cancel, editorParams) => {
+      //cell - the cell component for the editable cell
+      //onRendered - function to call when the editor has been rendered
+      //success - function to call to pass the successfuly updated value to Tabulator
+      //cancel - function to call to abort the edit and return to a normal cell
+      //editorParams - params object passed into the editorParams column definition property
+
+      //create and style editor
+      var editor = document.createElement("textarea");
+
+      //create and style input
+      editor.style.padding = "3px";
+      editor.style.width = "100%";
+      editor.style.height = "100%";
+      editor.style.boxSizing = "border-box";
+
+      //Set value of editor to the current value of the cell
+      editor.value = cell.getValue() || "";
+
+      //set focus on the select box when the editor is selected (timeout allows for editor to be added to DOM)
+      onRendered(function() {
+        editor.style.height = "100%";
+        editor.style.minHeight = "7em";
+        cell.getRow().normalizeHeight();
+        editor.focus();
+      });
+
+      //when the value has been set, trigger the cell to update
+      function successFunc() {
+        success(editor.value);
+        cell.getRow().normalizeHeight();
+      }
+
+      editor.addEventListener("change", successFunc);
+      editor.addEventListener("blur", successFunc);
+      editor.addEventListener("keyup", e => {
+        // esc
+        if (e.keyCode === 27) cancel();
+        // shift+enter
+        if (e.keyCode === 13 && e.shiftKey) success(editor.value);
+        // shift + tab
+        if (e.keyCode === 9 && e.shiftKey) cell.nav().left();
+      });
 
       //return the editor element
       return editor;
@@ -176,7 +239,8 @@ window.ENE.Editor = {
         {
           title: "Description",
           field: "description",
-          editor: "textarea",
+          editor: textAreaEditor,
+          variableHeight: true,
           formatter: "textarea",
           width: "40%"
         },
@@ -215,6 +279,7 @@ window.ENE.Editor = {
           field: "rule",
           editor: makeAutocompleteEditor(true),
           formatter: "textarea",
+          variableHeight: true,
           headerFilter: "input",
           cellEdited: cell => {
             if (cell.getOldValue() !== cell.getValue())
@@ -224,8 +289,9 @@ window.ENE.Editor = {
         {
           title: "Narrative",
           field: "narrative",
-          editor: "textarea",
-          formatter: "textarea"
+          editor: textAreaEditor,
+          formatter: "textarea",
+          variableHeight: true
         },
         {
           formatter: "buttonCross",
